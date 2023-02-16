4c917ee97594949f9dd72b8061546d2fc2f6bfd0
begaiyma <turdukeeva064@gmail.com>
2023-02-17 01:04:08 +0600

    I found the volume of the cylinder according to the formula

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ function functionName (b,t) {
   let result = b + t;
   console.log("My name is " + result);
 }
-functionName("Begaiym " , " Turdukeeva")
+functionName("Begaiym " , " Turdukeeva");
+
+function volume (height, radius) {
+  let result = height * 3.14 * radius ** 2 / 3 ;
+  console.log ("cylinder volume is " + result);
+} 
+volume (8, 5);
